451f0cbbc49021153dfb25faee0a6cb94d4739dc
godsonsunny10@gmail.com <godsonsunny190@gmail.com>
2025-08-07 21:42:03 +0530

    html up

---
+++ assets/javascript/slider.js
@@ -0,0 +1,22 @@
+/* ===================================== */
+/*         PROJECT SLIDER JS             */
+/* ===================================== */
+
+$(".projects_slider").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  // autoplay: true,
+  // autoplaySpeed: 2000,
+  arrows: false,
+  rows: 2,
+});
+
+/* ===================================== */
+/*           CLIENT SLIDER JS            */
+/* ===================================== */
+
+$(".client_slider").slick({
+  infinite: true,
+  slidesToShow: 7,
+  slidesToScroll: 4,
+});
