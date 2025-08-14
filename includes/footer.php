7061bf23e163f9ba5c0b50a3553b4604b8c30d67
godsonsunny10@gmail.com <godsonsunny190@gmail.com>
2025-08-14 19:03:14 +0530

    html up

---
+++ includes/footer.php
@@ -0,0 +1,128 @@
+<!-- FOOTER SECTION STARTS HERE -->
+<footer class="footer">
+    <div class="footer_wrapper">
+
+        <div class="footer_top">
+            <div class="container">
+                <div class="footer_flexTop">
+                    <div class="footer_col">
+                        <h4>Contact us for enquiries </h4>
+                        <h3>Pave the way forward with us</h3>
+                    </div>
+                    <div class="footer_col">
+                        <a href="">Let's talk <img src="./assets/images/icons/Path.png" alt=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer_middle">
+            <div class="container">
+                <div class="footer_flexCenter">
+                    <div class="footer_logo">
+                        <a href=""> <img src="./assets/images/logo/footerlogo@2x.png" alt=""></a>
+                        <p>Welcome to Nova Events — creating unforgettable experiences with creativity, innovation, and
+                            precision.
+                        </p>
+                    </div>
+
+                    <div class="footer_links">
+                        <h5>Company</h5>
+                        <ul>
+                            <li><a href="interior-design.html">About</a></li>
+                            <li><a href="urban-design.html">Services</a></li>
+                            <li><a href="decor-plan.html">Projects</a></li>
+                            <li><a href="decor-plan.html">Equipment Rental</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="footer_links">
+                        <h5>Quick Links</h5>
+                        <ul>
+                            <li><a href="urban-design.html">Services</a></li>
+                            <li><a href="decor-plan.html">Projects</a></li>
+                            <li><a href="decor-plan.html">Equipment Rental</a></li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="footer_links">
+                        <h5>Connect</h5>
+                        <ul>
+                            <li><a href="">info@alsayegh.com</a></li>
+                            <li><a href="">+971 2 449 9555 (Abu Dhabi)</a></li>
+                            <li><a href="">+971 4 444 1000 (Dubai)</a></li>
+                            <li><a href="">+971 50 657 0757 (Hotline)</a></li>
+                            <li><a href="">+966 56 464 7997 (Riyadh)</a></li>
+                            <li><a href="">+86 150 0180 9301 (Shanghai)</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer_bottom">
+            <div class="container">
+                <div class="footer_flexBottom">
+                    <h6>Copyright © 2023 Nova Events. Powerd By <a href=""><img src="./assets/images/webcastle1.png"
+                                alt=""></a>
+                    </h6>
+                    <ul>
+                        <li>
+                            <a href=""> <img src="./assets/images/icons/instagram.png" alt=""></a>
+                        </li>
+
+                        <li>
+                            <a href=""> <img src="./assets/images/icons/linkedin.png" alt=""></a>
+                        </li>
+
+                        <li>
+                            <a href=""> <img src="./assets/images/icons/twitter.png" alt=""></a>
+                        </li>
+
+                        <li>
+                            <a href=""> <img src="./assets/images/icons/youtube.png" alt=""></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- FOOTER SECTION ENDS HERE -->
+
+<div class="bg-lines">
+    <div class="bg-line-1"></div>
+    <div class="bg-line-2"></div>
+    <div class="bg-line-3"></div>
+    <div class="bg-line-4"></div>
+    <div class="bg-line-5"></div>
+</div>
+
+<!-- JQUERY CDN -->
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
+<!-- GSAP CDN -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"></script>
+
+<!-- BOOTSTRAP CDN -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"> </script>
+
+<!-- SWIPER CDN -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/8.4.7/swiper-bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+
+<!-- LENIS SMOOTH SCROLLING  -->
+<script src="https://unpkg.com/lenis@1.1.13/dist/lenis.min.js"></script>
+
+<!-- JS LINK -->
+<script src="./assets/javascript/gsap.js"></script>
+<script src="./assets/javascript/lenis.js"></script>
+<script src="./assets/javascript/slider.js"></script>
+<script src="./assets/javascript/script.js"></script>
+
+
+</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nova Events</title>
+
+    <!-- FAVICON  -->
+    <link rel="shortcut icon" href="./assets/images/fav/fav.svg" type="image/svg" sizes="32x32">
+
+    <!-- BOOTSTRAP LINK -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+
+    <!-- LENIS SMOOTH SCROLLING  -->
+    <link rel="stylesheet" href="https://unpkg.com/lenis@1.1.13/dist/lenis.css">
+
+    <!-- SWIPER CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/8.4.7/swiper-bundle.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css">
+
+    <!-- FONT STYLE LINK -->
+    <link rel="stylesheet" href="./assets/fonts/stylesheet.css">
+    <link rel="stylesheet" href="./assets/css/style.css.map">
+
+    <!-- CSS LINK -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<!-- HEADER SECTION STARTS HERE -->
+<header class="header">
+    <nav class="navbar navbar-expand-lg p-0" aria-label="Offcanvas navbar large">
+        <div class="container-fluid">
+            <div class="header_logo">
+                <a class="navbar-brand" href="#"><img src="./assets/images/logo/logo.png" alt=""></a>
+            </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar2"
+                aria-controls="offcanvasNavbar2">
+                <img src="./assets/images/icons/menu.png" alt="">
+            </button>
+            <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasNavbar2"
+                aria-labelledby="offcanvasNavbar2Label">
+                <div class="offcanvas-header">
+                    <div class="header_logo">
+                        <a href=""><img src="./assets/images/logo/footerlogo@2x.png" alt=""></a>
+                    </div>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
+                        aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <div class="header_menu">
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="service.html">Service</a></li>
+                            <li><a href="">Work</a></li>
+                            <li><a href="">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
+<!-- HEADER SECTION STARTS HERE -->
